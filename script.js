f7033f16ee936f17e17348da269c48aca2bfa64d
Ailwohs <juliaavancisoares@gmail.com>
2025-06-13 17:59:36 -0300

    adicionando o conteúdo

---
+++ script.js
@@ -0,0 +1,29 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+// Troca automática a cada 4 segundos
+setInterval(nextSlide, 4000);
+
+
+
+// LOJA
